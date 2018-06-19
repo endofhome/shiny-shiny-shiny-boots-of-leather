34707fb369f23b9717659fba05ff671f762009ee
Tom Barnsbury <ruralmyth@gmail.com>
2018-06-19 18:23:43 +0100

    Clean up the API a bit

--- src/main/kotlin/Gmailer.kt
+++ src/main/kotlin/Gmailer.kt
@@ -16,66 +16,89 @@ import java.io.BufferedReader
 import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
 import java.io.File
-import java.io.File.*
+import java.io.File.separator
 import java.nio.file.Files
 import java.nio.file.Paths
 import java.util.Properties
-import javax.mail.Message.RecipientType.*
+import javax.mail.Message.RecipientType.TO
 import javax.mail.Session
 import javax.mail.internet.InternetAddress
 import javax.mail.internet.MimeMessage
 
-class Gmailer(private val port: Int,
-              private val qString: String,
-              private val fromEmail: InternetAddress,
-              private val toEmail: InternetAddress
-) {
-    private val applicationName = "Kotlin Gmailer Bot"
-    private val jsonFactory = JacksonFactory.getDefaultInstance()
-    private val credentialsFolder = File("credentials")
-    private val scopes = listOf(GmailScopes.MAIL_GOOGLE_COM)
-    private val clientSecretPath = Paths.get(credentialsFolder.path + separator + "client_secret.json")
+class Gmailer(private val gmail: Gmail) {
+    private val user = "me"
 
-    fun findLastEmailWithSubjectAndReSendUsingNewDetails() {
-        val httpTransport = GoogleNetHttpTransport.newTrustedTransport()
-        val service = Gmail.Builder(httpTransport, jsonFactory, getCredentials(httpTransport))
-                .setApplicationName(applicationName)
-                .build()
+    companion object {
+        private const val applicationName = "Kotlin Gmailer Bot"
+        private val jsonFactory = JacksonFactory.getDefaultInstance()
+        private val credentialsFolder = File("credentials")
+        private val clientSecretPath = Paths.get(credentialsFolder.path + separator + "client_secret.json")
+        private val scopes = listOf(GmailScopes.MAIL_GOOGLE_COM)
 
-        val user = "me"
-        val messages: Messages = service.users().messages()
-        val listResponse = messages.list(user).setQ(qString).execute()
+        fun initialiseService(port: Int): Gmail {
+            val httpTransport = GoogleNetHttpTransport.newTrustedTransport()
+            return Gmail.Builder(httpTransport, jsonFactory, getCredentials(httpTransport, port))
+                    .setApplicationName(applicationName)
+                    .build()
+        }
+
+        private fun getCredentials(httpTransport: NetHttpTransport, port: Int): Credential {
+            val clientSecretReader: BufferedReader? = Files.newBufferedReader(clientSecretPath)
+            val clientSecrets: GoogleClientSecrets = GoogleClientSecrets.load(jsonFactory, clientSecretReader)
+
+            val flow = GoogleAuthorizationCodeFlow.Builder(
+                    httpTransport, jsonFactory, clientSecrets, scopes)
+                    .setDataStoreFactory(FileDataStoreFactory(credentialsFolder))
+                    .setAccessType("offline")
+                    .build()
+
+            val localServerReceiver = LocalServerReceiver.Builder().setPort(port).build()
+            return AuthorizationCodeInstalledApp(flow, localServerReceiver).authorize("user")
+        }
+    }
+
+
+    fun messages(): Messages {
+        return gmail.users().messages()
+    }
+
+    fun lastEmailForQuery(queryString: String): Message? {
+        val messages: Messages = messages()
+        val listResponse = messages.list(user).setQ(queryString).execute()
         // TODO: get the most recently received email, assuming it was for the month in question.
-        val firstMatch: Message = listResponse.messages.first()
-        val message = service.users().messages().get(user, firstMatch.id).setFormat("raw").execute()
-        val base64Url = Base64(true)
-        val emailBytes = base64Url.decode(message.raw)
+        return listResponse.messages.firstOrNull()
+    }
 
+    fun send(message: Message) {
+        messages().send(user, message).execute()
+    }
+
+    fun newMessageFrom(emailBytes: ByteArray?): MimeMessage {
         val props = Properties()
         val session = Session.getDefaultInstance(props, null)
-        val email: MimeMessage = MimeMessage(session, ByteArrayInputStream(emailBytes))
-        email.setFrom(fromEmail)
-        email.setRecipient(TO, toEmail)
-        val buffer = ByteArrayOutputStream()
-        email.writeTo(buffer)
-        val bytes = buffer.toByteArray()
-        val encodedEmail = Base64.encodeBase64URLSafeString(bytes)
-        val messageWithNewSendingData: Message = Message().setRaw(encodedEmail)
-
-        messages.send(user, messageWithNewSendingData).execute()
+        return MimeMessage(session, ByteArrayInputStream(emailBytes))
     }
 
-    private fun getCredentials(httpTransport: NetHttpTransport): Credential {
-        val clientSecretReader: BufferedReader? = Files.newBufferedReader(clientSecretPath)
-        val clientSecrets: GoogleClientSecrets = GoogleClientSecrets.load(jsonFactory, clientSecretReader)
-
-        val flow = GoogleAuthorizationCodeFlow.Builder(
-                httpTransport, jsonFactory, clientSecrets, scopes)
-                .setDataStoreFactory(FileDataStoreFactory(credentialsFolder))
-                .setAccessType("offline")
-                .build()
-
-        val localServerReceiver = LocalServerReceiver.Builder().setPort(port).build()
-        return AuthorizationCodeInstalledApp(flow, localServerReceiver).authorize("user")
+    fun rawMessageContent(firstMatch: Message): ByteArray? {
+        val message = gmail.users().messages().get(user, firstMatch.id).setFormat("raw").execute()
+        return Base64(true).decode(message.raw)
     }
+}
+
+fun MimeMessage.withSender(senderAddress: InternetAddress): MimeMessage {
+    this.setFrom(senderAddress)
+    return this
+}
+
+fun MimeMessage.withRecipient(recipientAddress: InternetAddress): MimeMessage {
+    this.setRecipient(TO, recipientAddress)
+    return this
+}
+
+fun MimeMessage.encode(): Message {
+    val buffer = ByteArrayOutputStream()
+    this.writeTo(buffer)
+    val bytes = buffer.toByteArray()
+    val encodedEmail = Base64.encodeBase64URLSafeString(bytes)
+    return Message().setRaw(encodedEmail)
 }
