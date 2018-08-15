4e57a8dafd71f1acb535433478a64e4bccd1bde2
Tom Barnsbury <ruralmyth@gmail.com>
2018-08-15 16:29:27 +0100

    Run NewsletterGmailer job from main function.

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,10 +1,15 @@
 import jobs.GmailForwarderJob.GmailForwarder
 import jobs.Job
+import jobs.NewsletterGmailerJob.NewsletterGmailer
 import java.time.ZonedDateTime
 
 fun main(args: Array<String>) {
     val gmailForwarder = GmailForwarder.initialise()
-    val jobs: List<Job> = listOf(gmailForwarder)
+    val newsletterGmailer = NewsletterGmailer.initialise()
+    val jobs: List<Job> = listOf(
+            gmailForwarder,
+            newsletterGmailer
+    )
 
     jobs.forEach { job ->
         val result = job.run(ZonedDateTime.now())
