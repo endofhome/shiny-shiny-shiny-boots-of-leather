be75e7fddbd5b090f213a9b4243d14aac8ea2283
Tom Barnsbury <ruralmyth@gmail.com>
2019-02-05 13:50:54 +0000

    Don't run cleaning rota gmailer job any more

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,11 +1,8 @@
-import MainConfigItem.MAIN_CLEANING_ROTA_GMAILER_JOB_NAME
 import MainConfigItem.MAIN_GMAIL_FORWARDER_JOB_NAME
 import config.Configurator
 import config.FormattedJobName
 import config.RequiredConfig
 import config.RequiredConfigItem
-import jobs.CleaningRotaGmailerJob.CleaningRotaGmailer
-import jobs.CleaningRotaGmailerJob.CleaningRotaGmailerConfig
 import jobs.GmailForwarderJob.GmailForwarder
 import jobs.GmailForwarderJob.GmailForwarderConfig
 import jobs.Job
@@ -16,13 +13,10 @@ fun main(args: Array<String>) {
     val mainConfig = Configurator(MainConfig(), Paths.get("credentials"))
     val mainJobName = mainConfig.requiredConfig.formattedJobName
     val gmailForwarderConfig = GmailForwarderConfig(mainConfig.get(MAIN_GMAIL_FORWARDER_JOB_NAME(mainJobName)))
-    val cleaningRotaGmailerConfig = CleaningRotaGmailerConfig(mainConfig.get(MAIN_CLEANING_ROTA_GMAILER_JOB_NAME(mainJobName)))
 
     val gmailForwarder = GmailForwarder.initialise(gmailForwarderConfig)
-    val cleaningRotaGmailer = CleaningRotaGmailer.initialise(cleaningRotaGmailerConfig)
     val jobs: List<Job> = listOf(
-            gmailForwarder,
-            cleaningRotaGmailer
+            gmailForwarder
     )
 
     jobs.forEach { job ->
@@ -34,12 +28,10 @@ fun main(args: Array<String>) {
 class MainConfig : RequiredConfig("Shiny Shiny Shiny Boots of Leather") {
     override fun values(): Set<RequiredConfigItem> =
         setOf(
-            MAIN_GMAIL_FORWARDER_JOB_NAME(formattedJobName),
-            MAIN_CLEANING_ROTA_GMAILER_JOB_NAME(formattedJobName)
+            MAIN_GMAIL_FORWARDER_JOB_NAME(formattedJobName)
         )
 }
 
 sealed class MainConfigItem(override val name: String) : RequiredConfigItem {
     class MAIN_GMAIL_FORWARDER_JOB_NAME(jobName: FormattedJobName) : MainConfigItem("${jobName.value}_GMAIL_FORWARDER_JOB_NAME")
-    class MAIN_CLEANING_ROTA_GMAILER_JOB_NAME(jobName: FormattedJobName) : MainConfigItem("${jobName.value}_CLEANING_ROTA_GMAILER_JOB_NAME")
 }
